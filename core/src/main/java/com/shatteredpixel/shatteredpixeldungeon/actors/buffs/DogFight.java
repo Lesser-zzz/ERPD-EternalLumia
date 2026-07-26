b10bcebd7e6431339cfe14437979a9c59ff75c9e
Lesser-zzz <shimada.gengi1257@gmail.com>
2026-07-26 16:09:37 +0900

    Add DogFight class for passive skill implementation
    Implement Hyunwoo's passive skill for DogFight.

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/DogFight.java
@@ -0,0 +1,13 @@
+/**
+ * Hyunwoo's passive skill.
+ *
+ * Gain stacks when attacking.
+ * Heal after reaching the required number of stacks.
+ */
+
+
+public class Dogfight {
+
+    private int attackCounter;
+
+}
