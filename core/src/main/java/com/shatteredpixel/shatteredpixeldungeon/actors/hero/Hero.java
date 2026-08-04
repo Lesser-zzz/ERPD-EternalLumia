69ac489d7e961f61c9c7ae4e09e837e6a72d52b8
ERer <shimada.gengi1257@gmail.com>
2026-08-04 23:58:41 +0900

    레벨업치트 손봄

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
@@ -1982,7 +1982,7 @@ public class Hero extends Char {
 
 	// [추가] 사지방 테스트용 즉시 레벨업 치트 함수
 	public void debugLevelUp() {
-    	this.lvlUp(); 
+    	this.earnExp( this.maxExp() - this.exp );
     	this.HP = this.HT; 
 	}
 	
